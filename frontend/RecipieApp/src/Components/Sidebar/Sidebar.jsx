2719b117e16c1a3a0080f290c75557a3bc3a1e1d
himanshu <pandahimanshu707@gmail.com>
2024-07-17 13:34:45 +0530

    modify sidebar

--- frontend/RecipieApp/src/Components/Sidebar/Sidebar.jsx
+++ frontend/RecipieApp/src/Components/Sidebar/Sidebar.jsx
@@ -5,30 +5,36 @@ import { Link, NavLink } from 'react-router-dom'
 function Header() {
   return (
     <>
-      <div class="header">
-        <div class="left-section">
-          <div>Logo</div>
-        </div>
-        <div class="right-section">
-          <div>User Icon</div>
-        </div>
-      </div>
 
-      <div class="sidebar">
-        <div class="sidebar-link">
+      <div className="sidebar">
+      <div className='logo'>Logo</div>
+        <div className="sidebar-link">
+        <i className="fa-solid fa-house"></i>
           <div>Home</div>
         </div>
-        <div class="sidebar-link">
+        <div className="sidebar-link">
+        <i className="fa-solid fa-magnifying-glass"></i>
           <div>Search</div>
         </div>
-        <div class="sidebar-link">
+        <div className="sidebar-link">
+          <i className="fa-solid fa-compass"></i>
           <div>Explore</div>
         </div>
-        <div class="sidebar-link">
+        <div className="sidebar-link">
+          <i className="fa-regular fa-square-plus"></i>
           <div>Create</div>
         </div>
-        <div class="sidebar-link">
+        <div className="sidebar-link">
+          <i className="fa-solid fa-floppy-disk"></i>
           <div>Saved</div>
+        </div>
+        <div className="sidebar-link">
+        <i className="fa-solid fa-floppy-disk"></i>
+        <div>Profile</div>
+        </div>
+        <div className="sidebar-link">
+        <i class="fa-solid fa-bars"></i>
+        <div>More</div>
         </div>
       </div>
     </>
